86a8653b6d8f4fc73e627df4d66fbe0f1de22d89
FamiliaMedika <khafidz.dpm@gmail.com>
2026-07-19 06:40:21 +0700

    Delete asesmen directory

--- asesmen/config.example.js
+++
@@ -1,6 +0,0 @@
-window.FM_CONFIG = {
-  SUPABASE_URL: "https://PROJECT_ID.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_REPLACE_ME",
-  APP_NAME: "RME Familia Medika",
-  PUBLIC_ASSESSMENT_URL: "https://www.familiamedika.id/asesmen/"
-};
