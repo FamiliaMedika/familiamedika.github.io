b894f58da25719f6e2bbafc52164a9cbce9b1a9c
FamiliaMedika <khafidz.dpm@gmail.com>
2026-08-12 10:25:19 +0700

    Add Sahabat Familia Supabase configuration

---
+++ sahabat/config.js
@@ -0,0 +1,9 @@
+window.SF_CONFIG = Object.freeze({
+  SUPABASE_URL: "https://cvfuuflnfexaqnncgjmw.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_mqJVZHJKwvtbT5xAJurv9w_j5LMqUlK",
+  APP_NAME: "Sahabat Familia",
+  APP_TAGLINE: "Kesehatan Keluarga, Lebih Dekat",
+  APP_PATH: "/sahabat/",
+  SUPPORT_WHATSAPP: "",
+  TERMS_VERSION: "2026-08-12"
+});
